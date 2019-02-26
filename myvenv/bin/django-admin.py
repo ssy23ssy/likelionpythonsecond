0c4e80352df441683a6103e1165273c68a138d6e
ssy23ssy <ssy23ssy@naver.com>
2019-02-27 00:16:21 +0900

    ready for deploy

---
+++ myvenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/ssr/Documents/django-workspace/likelion-django-blog/myvenv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
